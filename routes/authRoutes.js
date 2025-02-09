95eb17ade41dbe82b87c97a72061bd43a64e595e
Rohan Patel <rohanpatel1531@gmail.com>
2025-02-10 00:54:21 +0530

    empty commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,9 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+console.log("EMpty Commit");
+
+
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
